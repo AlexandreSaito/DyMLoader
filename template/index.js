01fb3e2c6bc41c8f1ec722dfc8f1458fa609b073
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-09-03 15:38:53 -0300

    Changed how get plugin class

--- template/index.js
+++ template/index.js
@@ -1,64 +1,60 @@
-const { Plugin } = require('../plugin.js');
+function getPlugin(plugin) {
+	
+    plugin.init = (trayMenu, mainMenu) => {
+        plugin.log('{module_name} init');
+    };
 
-const plugin = new Plugin();
+    plugin.beforePageLoad = () => {
+        plugin.log('{module_name} beforePageLoad');
+    };
 
-//setInterval(() => { console.log('Salve Akio!'); }, 1000)
+    plugin.onPageLoad = (page) => {
+        plugin.log('{module_name} onPageLoad');
 
-// check Electron for menu management
-// menu.click is removed if submenu is added
-plugin.init = (trayMenu, mainMenu) => {
-    plugin.log('{module_name} init');
-};
-
-plugin.beforePageLoad = () => {
-    plugin.log('{module_name} beforePageLoad');
-};
-
-plugin.onPageLoad = (page) => {
-    plugin.log('{module_name} onPageLoad');
-
-    page.addEventListener('click', '#btn-1', () => {
-        plugin.log("'#btn-1' was clicked")
-        page.getData(['#text-1', '#select-1', 'input[name="rdb-1"]', '#chk-1'], (data) => {
-            page.loadHtml('#list-data-getted', {
-                clearBeforeRender: false, // Can be ommited, default: true
-                fileOrHtml: [{ tag: 'div', text: `data received: ${JSON.stringify(data)}` }]
+        page.addEventListener('click', '#btn-1', () => {
+            plugin.log("'#btn-1' was clicked")
+            page.getData(['#text-1', '#select-1', 'input[name="rdb-1"]', '#chk-1'], (data) => {
+                page.loadHtml('#list-data-getted', {
+                    clearBeforeRender: false, // Can be ommited, default: true
+                    fileOrHtml: [{ tag: 'div', text: `data received: ${JSON.stringify(data)}` }]
+                });
             });
+
         });
 
-    });
+        page.addEventListener('change', '#select-1', () => {
+            plugin.log(`'#select-1' value has changed to {data}`)
+        });
 
-    page.addEventListener('change', '#select-1', () => {
-        plugin.log(`'#select-1' value has changed to {data}`)
-    });
+        page.addEventListener('change', 'input[name="rdb-1"]', (data) => {
+            plugin.log(`radio value has changed to ${data}"`);
+        });
 
-    page.addEventListener('change', 'input[name="rdb-1"]', (data) => {
-        plugin.log(`radio value has changed to ${data}"`);
-    });
+        page.addEventListener('change', '#chk-1', (data) => {
+            plugin.log(`check value has changed to ${data}`)
+        });
 
-    page.addEventListener('change', '#chk-1', (data) => {
-        plugin.log(`check value has changed to ${data}`)
-    });
+        page.loadHtml('#list-data-getted', {
+            clearBeforeRender: false, // Can be ommited, default: true
+            fileOrHtml: [{ tag: 'div', text: 'Content Load from script!' }]
+        });
 
-    page.loadHtml('#list-data-getted', {
-        clearBeforeRender: false, // Can be ommited, default: true
-        fileOrHtml: [{ tag: 'div', text: 'Content Load from script!' }]
-    });
+    };
 
-};
+    plugin.beforePageClose = (page) => {
+        plugin.log('{module_name} beforePageClose');
+    };
 
-plugin.beforePageClose = (page) => {
-    plugin.log('{module_name} beforePageClose');
-};
+    plugin.onPageClose = () => {
+        plugin.log('{module_name} onPageClose');
+    };
 
-plugin.onPageClose = () => {
-    plugin.log('{module_name} onPageClose');
-};
+    plugin.beforeQuit = () => {
+        plugin.log('{module_name} beforeQuit');
+    };
 
-plugin.beforeQuit = () => {
-    plugin.log('{module_name} beforeQuit');
-};
+}
 
 module.exports = {
-    plugin,
+    getPlugin,
 };
