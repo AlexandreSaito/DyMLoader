1f95a80e345046dfcb3a244d23ff2493a7b185e5
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-09-04 10:13:15 -0300

    Changed how to build HTML attributes to be more customizable

--- renderer/helperHtml.js
+++ renderer/helperHtml.js
@@ -15,12 +15,20 @@ export function makeHtml(parent, elements) {
 
             if (element.id) el.id = element.id;
             if (element.class) el.className = element.class;
-            if (element.type) el.setAttribute('type', element.type);
-            if (element.value) el.setAttribute('value', element.value);
-            if (element.name) el.setAttribute('name', element.name);
-            if (element.placeholder) el.setAttribute('placeholder', element.placeholder);
-            if (element.style) el.setAttribute('style', element.style);
             if (element.text) el.textContent = element.text;
+
+            let attrs = Object.getOwnPropertyNames(element);
+            for (let j = 0; j < attrs.length; j++) {
+                const attr = attrs[j];
+                if (attr == 'role' || attr == 'tag' || attr == 'id' || attr == 'text' || attr == 'class' || attr == 'children') continue;
+                el.setAttribute(attr, element[attr]);
+            }
+            // if (element.type) el.setAttribute('type', element.type);
+            // if (element.value) el.setAttribute('value', element.value);
+            // if (element.name) el.setAttribute('name', element.name);
+            // if (element.placeholder) el.setAttribute('placeholder', element.placeholder);
+            // if (element.style) el.setAttribute('style', element.style);
+            // if (element.accept) el.setAttribute('accept', element.accept);
 
             if (element.children) makeHtml(el, element.children);
 
@@ -40,6 +48,15 @@ export function getFormAsData(element) {
     for (let i = 0; i < input.length; i++) {
         const e = input[i];
         const name = e.getAttribute('name') ?? e.getAttribute('id');
+        if (e.getAttribute('type') == 'file') {
+            const fileData = [];
+            for (let j = 0; j < e.files.length; j++) {
+                const file = e.files[j];
+                fileData.push(api.utils.getPathForFile(file));
+            }
+            data[name] = fileData;
+            continue;
+        }
         data[name] = e.value;
     }
 
