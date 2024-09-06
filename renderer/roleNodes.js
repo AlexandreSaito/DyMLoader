e42d1dfb8b4951840e440c5bf8ad6eff13bb24d1
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-09-06 13:12:54 -0300

    Added new roles 'form', 'form-input' and 'form-select'

--- renderer/roleNodes.js
+++ renderer/roleNodes.js
@@ -8,12 +8,64 @@ const roleNodes = {
         node.textContent = 'Close';
         return node;
     },
-    'form-input': (input) => {
-
-    },
     'form': (form) => {
         const node = document.createElement('div');
         node.className = 'row';
+
+        return node;
+    },
+    'form-input': (input) => {
+        let inputNode = null;
+
+        if (!input.type) input.type = 'text'
+        if (input.type == 'textarea') {
+            inputNode = document.createElement('textarea');
+        } else {
+            inputNode = document.createElement('input');
+            inputNode.setAttribute('type', input.type);
+        }
+        if(input.type == 'file' && input.accept){
+            inputNode.setAttribute('accept', input.accept);
+        }
+        inputNode.className = 'form-control';
+        inputNode.name = input.name;
+
+        const node = document.createElement('div');
+        node.className = input.col ? `col-${input.col}` : 'col';
+
+        if (input.label) {
+            const label = document.createElement('label');
+            label.innerHTML = input.label;
+            node.append(label);
+        }
+
+        node.append(inputNode);
+
+        return node;
+    },
+    'form-select': (input) => {
+        const node = document.createElement('div');
+        node.className = input.col ? `col-${input.col}` : 'col';
+        if (input.label) {
+            const label = document.createElement('label');
+            label.innerHTML = input.label;
+            node.append(label);
+        }
+
+        const inputNode = document.createElement('select');
+        inputNode.className = 'form-select';
+        if (input.options && Array.isArray(input.options)) {
+            for (let i = 0; i < input.options; i++) {
+                const opt = document.createElement('option');
+                if(typeof input.options == 'string') {
+                    input.options = { value: input.options, label: input.options };
+                }
+                opt.value = input.options.value;
+                opt.innerHTML = input.options.label;
+                inputNode.append(opt);
+            }
+        }
+        node.append(inputNode);
 
         return node;
     }
