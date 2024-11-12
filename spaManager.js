754668fe48e6e0d63dda3f700bf3b48ab0ec7a2e
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-11-12 08:23:00 -0300

    Removed "ClearBeforeRender" from change HTML

--- spaManager.js
+++ spaManager.js
@@ -27,8 +27,8 @@ class Page {
         win.webContents.send('page-html', { id: this.id, parentQuery: parentQuery, clearBeforeRender: clearBeforeRender ?? true, html: typeof fileOrHtml == 'string' && fs.existsSync(fileOrHtml) ? fs.readFileSync(fileOrHtml).toString() : fileOrHtml })
     }
 
-    changeHtml(parentQuery, { clearBeforeRender, html }) {
-        win.webContents.send('page-html-change', { id: this.id, parentQuery: parentQuery, clearBeforeRender: clearBeforeRender ?? true, html: html })
+    changeHtml(parentQuery, { html }) {
+        win.webContents.send('page-html-change', { id: this.id, parentQuery: parentQuery, clearBeforeRender: true, html: html })
     }
 
     renderTable(parentQuery, table) {
