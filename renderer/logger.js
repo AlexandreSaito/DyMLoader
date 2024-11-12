0e1fb08743b172481d0d5da8b3a21682962de165
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-11-12 08:23:33 -0300

    Better view on logger

--- renderer/logger.js
+++ renderer/logger.js
@@ -4,10 +4,10 @@ const unique = document.getElementById('unique');
 export default function log(args) {
     const list = unique.querySelectorAll(`[value='${args.stack}']`);
     let html = `
-<a href="#" class="list-group-item" data-select="${args.stack}" style="display: ${list.length == 0 || !list[0].checked ? 'block' : 'none'}">
+<div class="list-group-item" data-select="${args.stack}" style="display: ${list.length == 0 || !list[0].checked ? 'block' : 'none'}">
     <p class="mb-1">${args.args.map(x => typeof x == 'object' ? JSON.stringify(x) : x)}</p>
     <small>${args.stack}</small>
-</a>
+</div>
     `;
 
     logger.innerHTML += html;
