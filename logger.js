6d898186ff0f878fd4da6fdb5ff697b7bcf3a308
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2024-11-12 08:22:25 -0300

    Fix logger when do not have stack to follow and show

--- logger.js
+++ logger.js
@@ -3,20 +3,28 @@ let win = null;
 
 function log(...args) {
     const stack = new Error().stack.split("at ");
+    const stackItem = stack[stack.length > 1 ? 2 : 1];
+
+    if (!stackItem) {
+        console.log('here');
+    }
 
     if (win) {
         win.webContents.send('log', { stack: stack[stack.length > 1 ? 2 : 1].trim(), args });
         return;
     }
 
-    console.log('\r\n----/----');
-    console.log(stack[stack.length > 1 ? 2 : 1].trim());
+    if (stackItem) {
+        console.log('\r\n----/----');
+        console.log(stackItem.trim());
+    }
     for (let arg of args) {
-        console.log(typeof arg == 'object' ? JSON.stringify(arg) : arg);
+        const argString = typeof arg == 'object' ? JSON.stringify(arg) : arg;
+        console.log(argString);
     }
 }
 
-function logProcess(...args){
+function logProcess(...args) {
     console.log('\r\n----/----');
     for (let arg of args) {
         console.log(typeof arg == 'object' ? JSON.stringify(arg) : arg);
